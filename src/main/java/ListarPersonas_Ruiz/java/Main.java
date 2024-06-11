2b7be86e07932e376e29a3e7311511636824b811
AgusNicole <163380438+AgusNicole@users.noreply.github.com>
2024-06-10 22:30:04 -0300

    Update Main.java
    Anteriormente estaba como Array, pero lo modifique con LIST.

--- src/main/java/ListarPersonas_Ruiz/java/Main.java
+++ src/main/java/ListarPersonas_Ruiz/java/Main.java
@@ -5,22 +5,22 @@ import javax.sound.midi.Soundbank;
 import java.util.Collection;
 import java.util.Comparator;
 import java.util.Arrays;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
 
      // Creo una lista de 5  personas, con array y instacio los objetos creandolos de la clase Persona.
-     Persona [] personas = {
-             new Persona("Pedro", "Sanchez"),
-             new Persona("Mirtha", "Legrand"),
-             new Persona("Bill", "Gates"),
-             new Persona("Ricardo", "Fort"),
-             new Persona("Lucia", "Perez"),
-     };
+     List<Persona> personas = new ArrayList<>();
+        personas.add(new Persona("Pedro", "Gutierrez"));
+        personas.add(new Persona("Lucia", "Gomez"));
+        personas.add(new Persona("Agustina", "Sanchez"));
+        personas.add(new Persona("Daniel", "Alarcon"));
+        personas.add(new Persona("Ricardo", "Fort"));
 
 
      //Comparo los nombres con el metodo sort y Comparator los ordeno alfabeticamente
-     Arrays.sort(personas, Comparator.comparing(Persona ::getNombre));
+    Collections.sort(personas, Comparator.comparing(Persona ::getNombre));
         System.out.println("Nombres Ordenados Alfabeticamente: ");
         for(Persona persona : personas) {
             System.out.println( persona.getNombre());
@@ -28,7 +28,7 @@ public class Main {
 
 
      //Comparo los apellidos con el metodo sort y Comparator los ordeno alfabeticamente
-     Arrays.sort(personas, Comparator.comparing(Persona::getApellido));
+ Collections.sort(personas, Comparator.comparing(Persona::getApellido));
         System.out.println("Apellidos Ordenados Alfabeticamente: ");
         for(Persona persona : personas) {
             System.out.println(persona.getApellido());
@@ -36,7 +36,7 @@ public class Main {
 
 
      //Comparo los apellido y los ordeno inversamente con el comparator.reverseOrder
-     Arrays.sort(personas, Comparator.comparing(Persona::getApellido, Comparator.reverseOrder()));
+     Collections.sort(personas, Comparator.comparing(Persona::getApellido, Comparator.reverseOrder()));
         System.out.println("Apellidos Ordenados a la Inversa: ");
         for(Persona persona : personas) {
             System.out.println(persona.getApellido());
